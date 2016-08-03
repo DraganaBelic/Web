16750f629aa00460a5d0c7977716b96ae6e41e99
Dragana Belic <draganabelic.mz@gmail.com>
2016-08-03 11:48:06 +0300

    test

---
+++ Library/src/by/tr/library/dao/impl/SQLAdminDao.java
@@ -0,0 +1,20 @@
+package by.tr.library.dao.impl;
+
+import by.tr.library.bean.Book;
+import by.tr.library.dao.AdminDao;
+
+public class SQLAdminDao implements AdminDao {
+
+	@Override
+	public boolean blockUser(int idUser) {
+		// stub
+		return false;
+	}
+
+	@Override
+	public boolean addNewBook(Book book) {
+		// stub
+		return false;
+	}
+
+}
