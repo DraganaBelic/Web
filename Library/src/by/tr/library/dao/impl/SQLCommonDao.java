a4c94e5fd2e5cde09dd49df91079d88e07d3ae79
Dragana Belic <draganabelic.mz@gmail.com>
2016-08-03 14:26:36 +0300

    add authorization

--- Library/src/by/tr/library/dao/impl/SQLCommonDao.java
+++ Library/src/by/tr/library/dao/impl/SQLCommonDao.java
@@ -3,7 +3,6 @@ package by.tr.library.dao.impl;
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -15,13 +14,12 @@ public class SQLCommonDao implements CommonDao {
 	@Override
 	public boolean authorization(String login, String password) throws DAOException {
 		// stub code
-		Connection conn=null;
-		PreparedStatement preparedStat= null;
-		String insertInTable = "INSERT INTO library_users " + " (`id`, `login`, `password`) VALUES " + "(?,?,?)";
-		String checkDuplikates="SELECT* FROM  LIBRARY_USERS WHERE login ='"+login+"'";
-		ResultSet rs= null;
+		Connection conn = null;
+		String checkDuplikates = "SELECT* FROM  LIBRARY_USERS WHERE login ='" + login + "'" + "&& password = '"
+				+ password + "'";
+		ResultSet rs = null;
 		Statement statement = null;
-		
+
 		try {
 			Class.forName("org.gjt.mm.mysql.Driver");
 		} catch (ClassNotFoundException e1) {
@@ -29,41 +27,26 @@ public class SQLCommonDao implements CommonDao {
 			e1.printStackTrace();
 			throw new DAOException("DAOExceptin message", e1);
 		}
-		
-		
-		
+
 		try {
-			conn=DriverManager.getConnection("jdbc:mysql://127.0.0.1/example", "root","12345");
-			statement=  conn.createStatement();
-			rs=statement.executeQuery(checkDuplikates);
-			
-			if (rs.next()) {
-				System.out.println("User already exist");
+			conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/example", "root", "12345");
+			statement = conn.createStatement();
+			rs = statement.executeQuery(checkDuplikates);
+
+			if (!rs.next()) {
+				System.out.println("Wrong username or password");
 				return false;
 			}
-			
-			preparedStat= conn.prepareStatement(insertInTable);
-			
-			preparedStat.setInt(1,0);
-			preparedStat.setString(2, login);
-			preparedStat.setString(3, password);
-			
-			
-			
-			preparedStat.executeUpdate();
-			System.out.println("Table library_users is updated !");
-			System.out.println("New user is added !");
-			
+
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			throw new DAOException("DAOExceptin message", e1);
-			
+
 		}
-		
-		
+
 		finally {
-			if (conn!=null) {
+			if (conn != null) {
 				try {
 					conn.close();
 				} catch (SQLException e) {
@@ -72,9 +55,9 @@ public class SQLCommonDao implements CommonDao {
 					throw new DAOException("DAOExceptin message", e);
 				}
 			}
-			if (preparedStat!=null) {
+			if (statement != null) {
 				try {
-					preparedStat.close();
+					statement.close();
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
